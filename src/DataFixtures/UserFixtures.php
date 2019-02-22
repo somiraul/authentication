0218f7a4141b46e66e6f03a04a6c08d105d1fba3
somiraul <raul.suria@searchoptmedia.com>
2019-02-22 19:44:29 +0800

    authentication

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('noreply@email.com');
+        $user->setPassword(
+          $this->encoder->encodePassword($user, '0000')
+        );
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
